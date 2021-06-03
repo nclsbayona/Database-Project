dcf66998de0aeda345495cc56abb70f54cdbe243
nclsbayona <59931437+nclsbayona@users.noreply.github.com>
2021-06-02 19:55:00 -0500

    see you tomorrow zzz

--- OnlineCarRentalSystem/src/NegocioOCR/FacadeOCR.java
+++ OnlineCarRentalSystem/src/NegocioOCR/FacadeOCR.java
@@ -141,12 +141,12 @@ public class FacadeOCR {
             query2 = (this.lineaControl.getEntityManager().createNativeQuery("SELECT * FROM LINEA WHERE CARROID=? AND RENTAID=?", Linea.class));
             query2.setParameter(1, (l.getObj().getCarroid().getId()));
             query2.setParameter(2, l.getObj().getRentaid());
-            Query query3=(this.parametroControl.getEntityManager().createNativeQuery("SELECT P.CANTIDAD_CARROS FROM PARAMETRO P, RENTA R WHERE R.ID=? AND R.PARAMETROID=P.ID"));
+            Query query3 = (this.parametroControl.getEntityManager().createNativeQuery("SELECT P.CANTIDAD_CARROS FROM PARAMETRO P, RENTA R WHERE R.ID=? AND R.PARAMETROID=P.ID"));
             query3.setParameter(1, l.getObj().getRentaid());
-            int parametro_cars_number=(int)(query3.getSingleResult());
-            Query query4=(this.parametroControl.getEntityManager().createNativeQuery("SELECT P.PORCENTAJE FROM PARAMETRO P, RENTA R WHERE R.ID=? AND R.PARAMETROID=P.ID"));
+            int parametro_cars_number = (int) (query3.getSingleResult());
+            Query query4 = (this.parametroControl.getEntityManager().createNativeQuery("SELECT P.PORCENTAJE FROM PARAMETRO P, RENTA R WHERE R.ID=? AND R.PARAMETROID=P.ID"));
             query4.setParameter(1, l.getObj().getRentaid());
-            int porcentaje_descuento_cars=(int)(query4.getSingleResult());
+            int porcentaje_descuento_cars = (int) (query4.getSingleResult());
             int cantidad_rentados;
             Linea the_line = null;
             try {
@@ -159,11 +159,11 @@ public class FacadeOCR {
                     double total_renta = 0;
                     double total_ingresado = 0;
                     double saldo_vueltos;
-                    cantidad_rentados=the_renta.getLineaCollection().size();
+                    cantidad_rentados = the_renta.getLineaCollection().size();
                     the_renta = (Renta) (query.getSingleResult());
                     for (Iterator<Linea> it = the_renta.getLineaCollection().iterator(); it.hasNext();) {
                         Linea line = it.next();
-                        int tmp=(line.getCantidad() * this.carroControl.consultarPrecioCarro(line.getCarroid().getId()))*((cantidad_rentados%parametro_cars_number)*);
+                        int tmp = (line.getCantidad() * this.carroControl.consultarPrecioCarro(line.getCarroid().getId())) * ((100) - ((cantidad_rentados % parametro_cars_number) * (porcentaje_descuento_cars)));
                         total_renta = total_renta + tmp;
                     }
                     for (Iterator<Rentaxbillete> it = the_renta.getRentaxbilleteCollection().iterator(); it.hasNext();) {
@@ -186,10 +186,10 @@ public class FacadeOCR {
                     this.lineaControl.create(linea);
                     l.setObj(linea);
                     the_renta.add(linea);
-                    cantidad_rentados=the_renta.getLineaCollection().size();
-                    for (Iterator<Linea> it = the_renta.getLineaCollection().iterator(); it.hasNext();) {
-                        Linea line = it.next();
-                        total_renta = total_renta + (line.getCantidad() * line.getCarroid().getPrecio());
+                    cantidad_rentados = the_renta.getLineaCollection().size();
+                    for (Linea line : this.rentaControl.getLineaCollection(the_renta.getId())) {
+                        int tmp = (line.getCantidad() * this.carroControl.consultarPrecioCarro(line.getCarroid().getId())) * ((100) - ((cantidad_rentados % parametro_cars_number) * (porcentaje_descuento_cars)));
+                        total_renta = total_renta + tmp;
                     }
                     for (Iterator<Rentaxbillete> it = the_renta.getRentaxbilleteCollection().iterator(); it.hasNext();) {
                         Rentaxbillete billeteIngresado = it.next();
